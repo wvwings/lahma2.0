f41a17d3895de10c592c60083fd5cdf02b645544
wvwings <112813282+wvwings@users.noreply.github.com>
2023-08-12 08:55:06 +0300

    Add files via upload

---
+++ bubl.js
@@ -0,0 +1,6 @@
+let firstname = prompt('Kak Vas Zovut') 
+ 
+if(firstname === 0 || firstname === ""){ 
+    alert('незнакомец')}else { 
+        alert(firstname + ' Привет, ты прошел тест ') 
+        }
